7c3ff4cda9f58ed7ec4667cbe9ac4f706135036c
Sebastian Hernandez <118933285+sebastianhernandezgualdron@users.noreply.github.com>
2024-05-21 15:56:09 -0500

    time validation :dragon:

--- app/Models/BibliotecaInscription.php
+++ app/Models/BibliotecaInscription.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use App\Http\Controllers\Controller;
+use App\Models\Service;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
@@ -30,35 +32,62 @@ class BibliotecaInscription extends Model
     public static function make(Request $request, $proj_id, $use_id)
     {
         $date = date('Y-m-d');
-
+        $services = DB::table('biblioteca_inscriptions as bi')
+            ->join('services as se', 'se.ser_id', '=', 'bi.ser_id')
+            ->select('se.ser_id', 'se.ser_date', 'se.ser_start', 'se.ser_end', 'se.ser_status')
+            ->where('bi.stu_id', '=', $request->stu_id)
+            ->where('se.ser_status', '=', 1)
+            ->get();
+        $service =  Service::find($request->ser_id);
+        $newSerStart = Carbon::parse($service->ser_start);
+        $newSerEnd = Carbon::parse($service->ser_end);
         // Se establecen los parametros para ingresar datos.
         $rules = [
-            'bio_ins_date' => ['required', 'regex:/^(\d{4})(\/|-)(0[1-9]|1[0-2])\2([0-2][0-9]|3[0-1])$/'],
             'stu_id' => ['required'],
             'ser_id' => ['required']
 
         ];
         // El sistema valida que estos datos sean correctos
-        if ($date <= $request->bio_ins_date) {
+        // if ($date <= $request->bio_ins_date) {
 
-            $validator = Validator::make($request->input(), $rules);
-            if ($validator->fails()) {
-                return response()->json([
-                    'status' => False,
-                    'message' => $validator->errors()->all()
-                ], 400);
-            } else {
-                // Si los datos son correctos se procede a guardar los datos en la base de datos.
-                $service =  Service::find($request->ser_id);
-                $student =  DB::table('students AS st')->where('st.stu_id', '=', $request->stu_id)->first();
-                if ($service == null || $student == null) {
-                    $message = ($service == null) ? 'El servicio no existe.' : ' El estudiante no existe.';
+
+        foreach ($services as $myService) {
+            // Pasamos los datos de la hora de reserva que llegan de la base de datos a tipo carbon
+
+            if ($myService->ser_date == $service->ser_date) {
+                $validatedSerStart = carbon::parse($myService->ser_start);
+                $validatedSerEnd = carbon::parse($myService->ser_end);
+                if ($newSerStart->lt($validatedSerEnd) && $newSerEnd->gt($validatedSerStart)) {
+                    // Hay superposición, la nueva reserva no es posible
                     return response()->json([
                         'status' => False,
-                        'message' => $message
+                        'message' => 'Ya se encuentra inscrito en un servicio en ese momento.'
                     ], 400);
                 }
+            }
+        }
+
+        $validator = Validator::make($request->input(), $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => False,
+                'message' => $validator->errors()->all()
+            ], 400);
+        } else {
+            // Si los datos son correctos se procede a guardar los datos en la base de datos.
+
+            $student =  DB::table('students AS st')->where('st.stu_id', '=', $request->stu_id)->first();
+            if ($service == null || $student == null) {
+                $message = ($service == null) ? 'El servicio no existe.' : ' El estudiante no existe.';
+                return response()->json([
+                    'status' => False,
+                    'message' => $message
+                ], 400);
+            }
+
+            if ($service->ser_status == 1) {
                 $biblioteca = new BibliotecaInscription($request->input());
+                $biblioteca->bio_ins_date = $date;
                 $biblioteca->bio_ins_status = 1;
                 $quotas = Service::substractQuote($request);
                 if ($quotas == true) {
@@ -76,92 +105,123 @@ class BibliotecaInscription extends Model
                         'message' => 'No ha sido posible crear la inscripción.'
                     ], 400);
                 }
+            } else {
+                return response()->json([
+                    'status' => False,
+                    'message' => 'Este servicio no está disponible.'
+                ], 400);
             }
-        } else {
-            return response()->json([
-                'status' => False,
-                'message' => 'Fecha invalida.'
-            ], 400);
         }
+        // } else {
+        //     return response()->json([
+        //         'status' => False,
+        //         'message' => 'Fecha invalida.'
+        //     ], 400);
+        // }
     }
 
     public static function Amend(Request $request, $proj_id, $use_id, $id)
     {
         $date = date('Y-m-d');
-
-
+        $services = DB::table('biblioteca_inscriptions as bi')
+            ->join('services as se', 'se.ser_id', '=', 'bi.ser_id')
+            ->select('bi.bio_ins_id','se.ser_id', 'se.ser_date', 'se.ser_start', 'se.ser_end', 'se.ser_status')
+            ->where('bi.stu_id', '=', $request->stu_id)
+            ->where('se.ser_status', '=', 1)
+            ->get();
+        $service =  Service::find($request->ser_id);
+        $newSerStart = Carbon::parse($service->ser_start);
+        $newSerEnd = Carbon::parse($service->ser_end);
         // Se establecen los parametros para ingresar datos.
         $rules = [
-            'bio_ins_date' => ['required', 'regex:/^(\d{4})(\/|-)(0[1-9]|1[0-2])\2([0-2][0-9]|3[0-1])$/'],
             'stu_id' => ['required'],
             'ser_id' => ['required']
 
         ];
         // El sistema valida que estos datos sean correctos
+        // if ($date <= $request->bio_ins_date) {
+        foreach ($services as $myService) {
+            // Pasamos los datos de la hora de reserva que llegan de la base de datos a tipo carbon
 
-        if ($date <= $request->bio_ins_date) {
-
-
-            $validator = Validator::make($request->input(), $rules);
-            if ($validator->fails()) {
-                return response()->json([
-                    'status' => False,
-                    'message' => $validator->errors()->all()
-                ], 400);
-            } else {
-
-                $biblioteca =  BibliotecaInscription::find($id);
-                $service =  Service::find($request->ser_id);
-                $student =  DB::table('students AS st')->where('st.stu_id', '=', $request->stu_id)->first();
-                if ($biblioteca == null || $service == null || $student == null) {
-                    $message = ($biblioteca == null) ? 'La inscripción no existe.' : ($service == null ? 'El servicio no existe.' : ' El estudiante no existe.');
+            if ($myService->ser_date == $service->ser_date && $myService->bio_ins_id != $id) {
+                $validatedSerStart = carbon::parse($myService->ser_start);
+                $validatedSerEnd = carbon::parse($myService->ser_end);
+                if ($newSerStart->lt($validatedSerEnd) && $newSerEnd->gt($validatedSerStart)) {
+                    // Hay superposición, la nueva reserva no es posible
                     return response()->json([
                         'status' => False,
-                        'message' => $message
+                        'message' => 'Ya se encuentra inscrito en un servicio en ese momento.'
                     ], 400);
-                } else {
-                    if ($biblioteca->ser_id == $request->ser_id) {
-                        // El usuario solo podrá cambiar el tipo de servicio
+                }
+            }
+        }
+        $validator = Validator::make($request->input(), $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => False,
+                'message' => $validator->errors()->all()
+            ], 400);
+        } else {
+
+            $biblioteca =  BibliotecaInscription::find($id);
+            $service =  Service::find($request->ser_id);
+            $student =  DB::table('students AS st')->where('st.stu_id', '=', $request->stu_id)->first();
+            if ($biblioteca == null || $service == null || $student == null) {
+                $message = ($biblioteca == null) ? 'La inscripción no existe.' : ($service == null ? 'El servicio no existe.' : ' El estudiante no existe.');
+                return response()->json([
+                    'status' => False,
+                    'message' => $message
+                ], 400);
+            } else {
+                // if ($biblioteca->ser_id == $request->ser_id) {
+                //     // El usuario solo podrá cambiar el tipo de servicio
+                //     $biblioteca->bio_ins_date = $date;
+                //     $biblioteca->ser_id = $request->ser_id;
+                //     $biblioteca->save();
+                //     Controller::NewRegisterTrigger("Se realizó una actualizacion de datos en la tabla biblioteca_Inscriptions ", 4, $proj_id, $use_id);
+                //     return response()->json([
+                //         'status' => True,
+                //         'message' => 'La inscripción se actualizó correctamente.',
+                //         'data' => $biblioteca
+                //     ], 200);
+                // } else {
+                if ($service->ser_status == 1) {
+                    $quotas = Service::substractQuote($request);
+                    // return $quotas;
+                    if ($quotas == false) {
+                        return response()->json([
+                            'status' => False,
+                            'message' => 'No hay cupos disponibles en este servicio.'
+                        ]);
+                    } else {
+
+                        // Se actualizará la fecha en el día que se genere el cambio.
+                        $biblioteca->bio_ins_date = $date;
+                        $biblioteca->stu_id = $request->stu_id;
                         $biblioteca->ser_id = $request->ser_id;
                         $biblioteca->save();
+                        // Se guarda la novedad en la base de datos.
                         Controller::NewRegisterTrigger("Se realizó una actualizacion de datos en la tabla biblioteca_Inscriptions ", 4, $proj_id, $use_id);
                         return response()->json([
                             'status' => True,
                             'message' => 'La inscripción se actualizó correctamente.',
                             'data' => $biblioteca
                         ], 200);
-                    } else {
-
-                        $quotas = Service::substractQuote($request);
-                        // return $quotas;
-                        if ($quotas == false) {
-                            return response()->json([
-                                'status' => False,
-                                'message' => 'No hay cupos disponibles en este servicio.'
-                            ]);
-                        } else {
-
-                            // Se actualizará la fecha en el día que se genere el cambio.
-                            $biblioteca->bio_ins_date = $date;
-                            $biblioteca->stu_id = $request->stu_id;
-                            $biblioteca->ser_id = $request->ser_id;
-                            $biblioteca->save();
-                            // Se guarda la novedad en la base de datos.
-                            Controller::NewRegisterTrigger("Se realizó una actualizacion de datos en la tabla biblioteca_Inscriptions ", 4, $proj_id, $use_id);
-                            return response()->json([
-                                'status' => True,
-                                'message' => 'La inscripción se actualizó correctamente.',
-                                'data' => $biblioteca
-                            ], 200);
-                        }
                     }
+                } else {
+                    return response()->json([
+                        'status' => False,
+                        'message' => 'Este servicio no está disponible.'
+                    ]);
                 }
+                // }7
             }
-        } else {
-            return response()->json([
-                'status' => False,
-                'message' => 'Fecha invalida.'
-            ], 400);
         }
+        // } else {
+        //     return response()->json([
+        //         'status' => False,
+        //         'message' => 'Fecha invalida.'
+        //     ], 400);
+        // }
     }
 }
