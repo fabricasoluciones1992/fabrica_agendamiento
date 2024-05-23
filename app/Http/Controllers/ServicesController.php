e8c8943bfaab6238d1b06f9d96559dbfa9eba12d
sebastianhernandezgualdron <118933285+sebastianhernandezgualdron@users.noreply.github.com>
2024-05-23 09:45:02 -0500

    :fu:

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -37,8 +37,8 @@ class ServicesController extends Controller
 
     public function store(Request $request,$proj_id, $use_id)
     {
-        $rules = [
-            'ser_name' => 'regex:/^[A-ZÁÉÍÓÚÜÀÈÌÒÙÑ\s]+$/',
+        $rules = [  
+            'ser_name' => ['required','regex:/^[ÁÉÍÓÚÜÑA-Z\s\W]+$/'],
             'ser_date' => ['required', 'regex:/^(\d{4})(\/|-)(0[1-9]|1[0-2])\2([0-2][0-9]|3[0-1])$/'],
             'ser_start' => ['required', 'regex:/^([0-1][0-9]|2[0-3])(:)([0-5][0-9])$/'],
             'ser_end' => ['required', 'regex:/^([0-1][0-9]|2[0-3])(:)([0-5][0-9])$/'],
